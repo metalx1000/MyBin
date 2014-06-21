ded9abaf44900e97c6c265688a9ecb3430de287a
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-06-21 12:27:19 -0400

    Generated Python code for GRC files

---
+++ gnuradio/fm_radio.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Top Block
+# Generated: Sat Jun 21 12:26:51 2014
+##################################################
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import wx
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.variable_slider_0 = variable_slider_0 = 89.5e6
+        self.samp_rate = samp_rate = 2000000
+        self.freq = freq = variable_slider_0
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="Waterfall Plot",
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
+        _variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._variable_slider_0_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_sizer,
+        	value=self.variable_slider_0,
+        	callback=self.set_variable_slider_0,
+        	label='variable_slider_0',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._variable_slider_0_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_sizer,
+        	value=self.variable_slider_0,
+        	callback=self.set_variable_slider_0,
+        	minimum=80e6,
+        	maximum=120e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_variable_slider_0_sizer)
+        self.rtlsdr_source_1 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.rtlsdr_source_1.set_sample_rate(samp_rate)
+        self.rtlsdr_source_1.set_center_freq(freq, 0)
+        self.rtlsdr_source_1.set_freq_corr(0, 0)
+        self.rtlsdr_source_1.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_1.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_1.set_gain_mode(False, 0)
+        self.rtlsdr_source_1.set_gain(10, 0)
+        self.rtlsdr_source_1.set_if_gain(20, 0)
+        self.rtlsdr_source_1.set_bb_gain(20, 0)
+        self.rtlsdr_source_1.set_antenna("", 0)
+        self.rtlsdr_source_1.set_bandwidth(0, 0)
+          
+        self.rational_resampler_xxx_1 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=500,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=4,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, 100000, 1e6, firdes.WIN_HAMMING, 6.76))
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=500000,
+        	audio_decimation=1,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.rational_resampler_xxx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_1, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.audio_sink_0, 0))
+        self.connect((self.rtlsdr_source_1, 0), (self.wxgui_waterfallsink2_0, 0))
+        self.connect((self.rtlsdr_source_1, 0), (self.rational_resampler_xxx_0, 0))
+
+
+# QT sink close method reimplementation
+
+    def get_variable_slider_0(self):
+        return self.variable_slider_0
+
+    def set_variable_slider_0(self, variable_slider_0):
+        self.variable_slider_0 = variable_slider_0
+        self.set_freq(self.variable_slider_0)
+        self._variable_slider_0_slider.set_value(self.variable_slider_0)
+        self._variable_slider_0_text_box.set_value(self.variable_slider_0)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100000, 1e6, firdes.WIN_HAMMING, 6.76))
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.rtlsdr_source_1.set_sample_rate(self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.rtlsdr_source_1.set_center_freq(self.freq, 0)
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = top_block()
+    tb.Start(True)
+    tb.Wait()
+
