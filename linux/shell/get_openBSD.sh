511404c602face6883ba6d4f9cbce69b37014ef8
metalx1000 <metal_bullets@filmsbykris.com>
2014-07-19 17:54:07 -0400

    Added script that get's openBSD and loads it into qemu

---
+++ linux/shell/get_openBSD.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+iso="openbsd.iso"
+img="openbsd.img"
+
+url="http://openbsd.mirrorcatalogs.com/pub/OpenBSD/5.5/i386/cd55.iso"
+
+clear
+echo "Getting install ISO...\n"
+
+#get ISO http://www.openbsd.org/
+wget -c "$url" -O $iso 
+
+#create Empty IMG
+if [ -a $img ]
+then
+    echo "$img image already exists"
+else
+    echo "\nCreating $img for install...\n"
+    dd bs=512 count=5400000 if=/dev/zero of=$img
+fi
+
+#do install in qemu
+qemu-system-i386 -boot d -cdrom $iso -hda $img
+
+clear
+###############NOTES##########
+echo "
+NOTES on Package manager for openBSD
+http://www.cyberciti.biz/tips/openbsd-add-package-howto.html
+http://bsdly.blogspot.com/2013/04/youve-installed-it-now-what-packages.html
+# export PKG_PATH=ftp://ftp.openbsd.org/pub/OpenBSD/`uname -r`/packages/`uname -m`/
+# pkg_add -v -i links
+# pkg_info 
+
+########Update existing package######
+# pkg_add -u links
+
+##########update all package######
+# pkg_add
+"
+
+#run system in qemu
+qemu-system-i386 -hda $img
