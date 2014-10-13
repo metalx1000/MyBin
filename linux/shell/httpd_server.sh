76d5f8df4674e738fb657ac7e763752c5265f2f2
metalx1000 <metalx1000@users.noreply.github.com>
2014-10-13 16:08:23 -0400

    Update httpd_server.sh

--- linux/shell/httpd_server.sh
+++ linux/shell/httpd_server.sh
@@ -9,3 +9,14 @@ busybox httpd -p 8080 -v -f
 #don't forget to chmod +x cgi files
 #'-f' Do not daemonize
 #more info at http://wiki.openwrt.org/doc/howto/http.httpd
+
+#example config:
+#busybox httpd -p 8080 -v -f -c http.conf
+#H:/tmp/web
+#A:127.0.0.1
+#D:*
+#*.cgi:/bin/sh
+#example user is bob and password is linux2
+#/:bob/linux2
+#password as hash httpd -m "linux2"
+#/:bob:$1$1mWbCBYu$3Yp2f6Kao5SiEdW.f/WEv0
