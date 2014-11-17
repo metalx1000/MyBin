f6a3b2c55d0c29b6f23c6e87b2aa29c0556c7588
metalx1000 <metalx1000@users.noreply.github.com>
2014-11-17 15:29:50 -0500

    Update debian_chroot.sh

--- android/debian_chroot.sh
+++ android/debian_chroot.sh
@@ -32,4 +32,14 @@ busybox mount --bind /dev dev
 mount -t devpts devpts dev/pts
 mount -t proc proc proc
 mount -t sysfs sysfs sys
+busybox sysctl -w net.ipv4.ip_forward=1
+
+echo '
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844
+' > $mount/etc/resolv.conf
+
+
 busybox chroot . /bin/bash
