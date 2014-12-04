db654fa2ceeea1b3e094f3709f5ca6fbd358a23e
metalx1000 <metalx@gator4018.hostgator.com>
2014-12-04 10:18:58 -0500

    Added HASH tags

--- JavaScript/fbk_share/index.php
+++ JavaScript/fbk_share/index.php
@@ -10,6 +10,14 @@
                 var line = lines[r].split('|');
                 title = line[0];
                 title = title.replace("#", "-");
+
+                var a = ["linux", "bash", "python", "programming", "tutorial","shell","gtk","qt", "grep","awk","blender", "ThreeJS", "HTML5", "BabylonJS", "Kdenlive","Imagemagick","GIMP","Ardour","hydrogen","pygame","slitaz","webkit","Windows","newcat","GUI"];
+                a.forEach(function(entry) {
+                     var regex = new RegExp( '(' + entry + ')', 'gi' );
+                     title=title.replace( regex, "#$1" );
+                    //title = title.replace("(" + entry + ")"gi, '#$1');
+                });
+                title2 = title.replace("#", '%23');
                 id = line[1];
                 $("#result").html( "<h1>" + title + "</h1>" );
                 $("#result").append("<img id='go' src=http://i3.ytimg.com/vi/" + id + "/hqdefault.jpg><br>");
@@ -18,8 +26,8 @@
    
             $("#result").on('click', '#go', function(){
                 window.open("https://plus.google.com/share?url=https://www.youtube.com/watch?v="+id);
-                window.open("https://twitter.com/intent/tweet?url=https://www.youtube.com/watch?v="+id+"&text="+title+":&via=YouTube&related=YouTube,YouTubeTrends,YTCreators");
-                window.open("https://www.blogger.com/blog-this.g?n="+title+"&source=youtube&b=%3Ciframe+width=%22480%22+height=%22270%22+src=%22https://www.youtube.com/embed/"+id+"%22+frameborder=%220%22+allowfullscreen%3E%3C/iframe%3E&eurl=https://i.ytimg.com/vi/"+id+"/hqdefault.jpg");
+                window.open("https://twitter.com/intent/tweet?url=https://www.youtube.com/watch?v="+id+"&text="+title2+":&via=YouTube&related=YouTube,YouTubeTrends,YTCreators");
+                window.open("https://www.blogger.com/blog-this.g?n="+title2+"&source=youtube&b=%3Ciframe+width=%22480%22+height=%22270%22+src=%22https://www.youtube.com/embed/"+id+"%22+frameborder=%220%22+allowfullscreen%3E%3C/iframe%3E&eurl=https://i.ytimg.com/vi/"+id+"/hqdefault.jpg");
 
                 window.open("https://www.facebook.com/pages/Films-By-Kris/225113590836253");
             });
